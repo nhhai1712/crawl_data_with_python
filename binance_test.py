03ede18e6b43efb480d1e2de09bfa2546337022a
nhhai1712 <hai.nh204963@sis.hust.edu.vn>
2023-11-29 10:46:56 +0700

    update

--- binance_test.py
+++
@@ -1,5 +0,0 @@
-def binance_test(): 
-    print("binance")
-
-if __name__ == "__binance__":
-    binance_test()
--- rarible_test.py
+++
@@ -1,5 +0,0 @@
-def rarible_test():
-    print("rarible")
-
-if __name__ == "__rarible__":
-    rarible_test()
